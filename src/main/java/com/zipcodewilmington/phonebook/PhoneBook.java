f0ad0952dc44f97c0b4d94711d15ae89c3e36019
Davis Perchik <davisperchik@zipcoders-MacBook-Pro.local>
2019-02-14 18:25:48 -0500

    Methods and Tests working up to Display

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,51 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.Set;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private TreeMap<String, String> inputs;
+
+
+    public PhoneBook() {
+        inputs = new TreeMap<String, String>();
+    }
+
+
+    public void add(String name, String number) {
+        inputs.put(name, number);
+    }
+
+    public String lookup(String name) {
+        return inputs.get(name);
+    }
+
+    public void remove(String name) {
+        inputs.remove(name);
+    }
+
+    public Integer getSize() {
+        return inputs.size();
+    }
+
+    public String hasname(String name) {
+        return inputs.get(name);
+    }
+
+    public String hasnumber(String number) {
+        Set<String> nameList = inputs.keySet();
+        for (String name : nameList) {
+            if (inputs.get(name).equals(number))
+                return name;
+        }
+        return "Not Found";
+    }
+
+    public void clear() {
+        inputs.clear();
+    }
 }
+
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,80 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+    PhoneBook phoneBook;
+    @Before
+    public void Setup(){
+        phoneBook = new PhoneBook();
+        phoneBook.add("Davis", "123-456-7890");
+    }
+
+    @After
+    public void tearDown(){
+        phoneBook.clear();
+    }
+
+    @Test
+    public void testAdd(){
+        //Given
+        String expected = "123-456-7890";
+
+        //When
+        phoneBook.lookup("Davis");
+
+        //Then
+        Assert.assertEquals(expected, phoneBook.lookup("Davis"));
+    }
+
+    @Test
+    public void testRemove1(){
+        //Given
+        phoneBook.remove("Davis");
+        Integer expected = 0;
+
+        //When
+        phoneBook.getSize();
+
+        //Then
+        Assert.assertEquals(expected, phoneBook.getSize());
+    }
+
+    @Test
+    public void testRemove2(){
+        //Given
+
+    }
+
+    @Test
+    public void testLookup(){
+        //Given
+        String  expected = "123-456-7890";
+
+        //When
+        phoneBook.hasname("Davis");
+
+        //Then
+        Assert.assertEquals(expected, phoneBook.hasname("Davis"));
+    }
+
+    @Test
+    public void testReverseLookup(){
+        //Given
+        String expected = "Davis";
+
+        //When
+        phoneBook.hasnumber("123-456-7890");
+
+        //Then
+        Assert.assertEquals(expected, phoneBook.hasnumber("123-456-7890"));
+    }
 }
